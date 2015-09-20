e8f8c7a153314007a4d03c30c4c67ef3d7e52633
Matej Kollar <208115@mail.muni.cz>
2015-09-20 20:34:12 +0200

    Polish example

--- example/xmonad.hs
+++ example/xmonad.hs
@@ -1,16 +1,13 @@
 module Main (main) where
 
-import System.IO (Handle, hPutStrLn)
-
 import XMonad (xmonad, (<+>))
 import XMonad.Util.Run (spawnPipe)
 
-import My.XMonad.Config (myConfig)
 import My.XMonad.Config.Compositing (addCompositing)
+import My.XMonad.Config (myConfig)
+import My.XMonad.Config.Tools (update, keys)
 import My.XMonad.Config.Xmobar (addXmobar)
 import My.XMonad.Multimedia.Amixer (alsaKeys)
-
-import My.XMonad.Config.Tools (update, keys)
 
 
 main :: IO ()
