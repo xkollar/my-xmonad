a168b8b7e5baf3b86859911ff7c8642fa150faea
Matej Kollar <208115@mail.muni.cz>
2016-06-10 08:22:06 +0200

    Some polish
    Better help.
    Saner handling of options.

--- helper/gen_current_layout.sh
+++ helper/gen_current_layout.sh
@@ -2,21 +2,29 @@
 
 function usage() {
     echo "Usage: ${0} <full|brief|brief-color> [filename]"
+    echo "       ${0} <full|brief|brief-color> [ImageMagick options] filename"
+    echo "       ${0} <--help|-h>"
 }
 
 function main() {
-    case "${1}" in
+    local -r mode="${1}"; shift
+
+    case "${mode}" in
         full | brief | brief-color )
             true
             ;;
-        * )
+        --help | -h )
             usage
+            exit 0
+            ;;
+        * )
+            usage >&2
             exit 1
             ;;
     esac
 
-    sed -f <( xmodmap -pke | sed -f script.sed ) "ibm-pc-keyboard-${1}.svg" \
-    | convert -background transparent - ${2:-x:}
+    sed -f <( xmodmap -pke | sed -f script.sed ) "ibm-pc-keyboard-${mode}.svg" \
+    | convert -background transparent - "${@:-x:}"
 }
 
 main "${@}"
